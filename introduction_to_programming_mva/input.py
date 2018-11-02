a2fc0ae5f23df781d9fb6038a380aad9d984f577
Bhagyaraj Chitoth <Bhagyarajc@gmail.com>
2018-11-02 16:10:03 +0530

    first few programs

---
+++ introduction_to_programming_mva/input.py
@@ -0,0 +1,12 @@
+'''
+This sample program has been created as part of the Microsoft Virual Academy Course
+Introduction to Python Programming
+'''
+
+# accepting user input and returns the types information to the variable
+name = input('What is your name? ')
+
+# printing the name
+print(name)
+
+
