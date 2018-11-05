7cec01906d3bec738da3dc3661a92c30bc55931d
Bhagyaraj Chitoth <Bhagyarajc@gmail.com>
2018-11-05 08:18:07 +0530

    functions introduction

---
+++ introduction_to_programming_mva/functions_introduction.py
@@ -0,0 +1,12 @@
+'''
+    This script has been created as part of the introduction to python training by microsoft virtual academy
+    This section covers how to make functions
+'''
+
+def printMessage(userName):
+    print('\nHello ' + userName)
+    return
+
+userName = input('Enter your name: ')
+printMessage(userName)
+
